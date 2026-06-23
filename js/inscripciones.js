47ee06bf01aa97012123be5fd6b60489ab306026
sheyla astorga <sheylaastorga1998@gmail.com>
2026-06-23 10:24:50 -0300

    Verficique los cambios en los botones de accion de inscripcion y borre los estados completamente de el html y de js

--- js/inscripciones.js
+++ js/inscripciones.js
@@ -3,10 +3,8 @@ const API_URL = "http://localhost:3000";
 const selecEvento = document.getElementById("selecEvento");
 const selecParticipante = document.getElementById("selecParticipante");
 const filtroEvento = document.getElementById("filtroEvento");
-const filtroEstado = document.getElementById("filtroEstado");
 const tablaInscripciones = document.getElementById("tabla-inscripciones");
 const btnGuardar = document.getElementById("btnGuardarInscripcion");
-const selecEstadoModal = document.getElementById("estadoInscripcion");
 
 let eventos = [];
 let participantes = [];
@@ -17,20 +15,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     btnGuardar.addEventListener("click", crearInscripcion);
     filtroEvento.addEventListener("change", mostrarInscripciones);
-    filtroEstado.addEventListener("change", mostrarInscripciones);
 
-    const modalElement = document.getElementById('modalInscripcion')
-    modalElement.addEventListener('hidden.bs.modal', () =>{
-        selecEvento.value = '';
-        selecParticipante.value = ''; 
-        selecEstadoModal.value = 'Confirmado';
+    const modalElement = document.getElementById("modalInscripcion");
 
-        btnGuardar.textContent = 'Guardar';
-        btnGuardar.onclick = null; 
-        btnGuardar.removeEventListener("click", crearInscripcion); 
+    modalElement.addEventListener("hidden.bs.modal", () => {
+        selecEvento.value = "";
+        selecParticipante.value = "";
+
+        btnGuardar.textContent = "Guardar";
+        btnGuardar.onclick = null;
+
+        btnGuardar.removeEventListener("click", crearInscripcion);
         btnGuardar.addEventListener("click", crearInscripcion);
-
-    })
+    });
 });
 
 async function cargarDatosIniciales() {
@@ -96,38 +93,38 @@ async function crearInscripcion() {
         return;
     }
 
-    //Validacion para que solo se puden inscribir a un evento
+    try {
+        const respuestaValidacion = await axios.get(
+            `${API_URL}/inscripciones?eventoId=${eventoId}&participanteId=${participanteId}`
+        );
 
-    try{
-        const respuestaValidacion = await axios.get(`${API_URL}/inscripciones?eventoId=${eventoId}&participanteId=${participanteId}`)
-
-        if(respuestaValidacion.data.length > 0){
-            alert ('Este participante ya se encuentra inscripto en este evento.')
+        if (respuestaValidacion.data.length > 0) {
+            alert("Este participante ya se encuentra inscripto en este evento.");
             return;
         }
-    }catch (error){
-        console.log('Error al validar duplicados: ', error)
-        alert('Hubo un problema al verificar la inscripción')
+    } catch (error) {
+        console.log("Error al validar duplicados:", error);
+        alert("Hubo un problema al verificar la inscripción");
         return;
     }
 
     const datos = {
-        eventoId: (eventoId),
-        participanteId: (participanteId),
-        fecha: new Date().toLocaleDateString(),
-        estado: selecEstadoModal.value
+        eventoId,
+        participanteId,
+        fecha: new Date().toLocaleDateString()
     };
 
     await axios.post(`${API_URL}/inscripciones`, datos);
 
     filtroEvento.value = eventoId;
-    filtroEstado.value = "";
     selecEvento.value = "";
     selecParticipante.value = "";
 
     await mostrarInscripciones();
 
-    const modal = bootstrap.Modal.getInstance(document.getElementById("modalInscripcion"));
+    const modal = bootstrap.Modal.getInstance(
+        document.getElementById("modalInscripcion")
+    );
 
     if (modal) {
         modal.hide();
@@ -136,7 +133,6 @@ async function crearInscripcion() {
 
 async function mostrarInscripciones() {
     const eventoId = filtroEvento.value;
-    const estado = filtroEstado.value;
 
     tablaInscripciones.innerHTML = "";
 
@@ -145,14 +141,12 @@ async function mostrarInscripciones() {
     if (eventoId === "") {
         respuesta = await axios.get(`${API_URL}/inscripciones`);
     } else {
-        respuesta = await axios.get(`${API_URL}/inscripciones?eventoId=${(eventoId)}`);
+        respuesta = await axios.get(
+            `${API_URL}/inscripciones?eventoId=${eventoId}`
+        );
     }
 
-    let inscripciones = respuesta.data;
-
-    if (estado !== "") {
-        inscripciones = inscripciones.filter(inscripcion => inscripcion.estado === estado);
-    }
+    const inscripciones = respuesta.data;
 
     inscripciones.forEach(inscripcion => {
         mostrarFilaInscripcion(inscripcion);
@@ -160,89 +154,88 @@ async function mostrarInscripciones() {
 }
 
 function mostrarFilaInscripcion(inscripcion) {
-    const participante = participantes.find(participante => participante.id == inscripcion.participanteId);
-    const evento = eventos.find(evento => evento.id == inscripcion.eventoId);
+    const participante = participantes.find(
+        participante => participante.id == inscripcion.participanteId
+    );
+
+    const evento = eventos.find(
+        evento => evento.id == inscripcion.eventoId
+    );
 
     const fila = document.createElement("tr");
 
     const tdParticipante = document.createElement("td");
-    tdParticipante.textContent = participante ? participante.nombre : "Sin participante";
+    tdParticipante.textContent = participante
+        ? participante.nombre
+        : "Sin participante";
 
     const tdEvento = document.createElement("td");
-    tdEvento.textContent = evento ? evento.nombre : "Sin evento";
+    tdEvento.textContent = evento
+        ? evento.nombre
+        : "Sin evento";
 
     const tdFecha = document.createElement("td");
     tdFecha.textContent = inscripcion.fecha;
 
-    const tdEstado = document.createElement("td");
-    const badgeEstado = document.createElement("span");
-    badgeEstado.textContent = inscripcion.estado;
-    badgeEstado.className = obtenerClaseEstado(inscripcion.estado);
-    tdEstado.appendChild(badgeEstado);
-
     const tdAcciones = document.createElement("td");
+
     tdAcciones.innerHTML = `
-    <div class="d-flex gap-2">
-        <button type="button" class="btn btn-warning btn-sm" onclick="actualizarAsistencia('${inscripcion.id}')">Editar</button>
-        <button type="button" class="btn btn-danger btn-sm" onclick="cancelarInscripcion('${inscripcion.id}')">Eliminar</button>
-    </div>
-`;
+        <div class="d-flex gap-2">
+            <button
+                type="button"
+                class="btn btn-warning btn-sm"
+                onclick="actualizarAsistencia('${inscripcion.id}')">
+                Editar
+            </button>
+
+            <button
+                type="button"
+                class="btn btn-danger btn-sm"
+                onclick="cancelarInscripcion('${inscripcion.id}')">
+                Eliminar
+            </button>
+        </div>
+    `;
 
     fila.appendChild(tdParticipante);
     fila.appendChild(tdEvento);
     fila.appendChild(tdFecha);
-    fila.appendChild(tdEstado);
     fila.appendChild(tdAcciones);
 
     tablaInscripciones.appendChild(fila);
 }
 
-function obtenerClaseEstado(estado) {
-    if (estado === "Confirmado") {
-        return "badge bg-success";
-    }
-
-    if (estado === "Asistió") {
-        return "badge bg-primary";
-    }
-
-    if (estado === "Ausente") {
-        return "badge bg-danger";
-    }
-
-    return "badge bg-secondary";
-}
-
 async function actualizarAsistencia(idInscripcion) {
     try {
-        
-        const respuesta = await axios.get(`${API_URL}/inscripciones/${idInscripcion}`);
+        const respuesta = await axios.get(
+            `${API_URL}/inscripciones/${idInscripcion}`
+        );
+
         const inscripcion = respuesta.data;
-        
+
         selecEvento.value = inscripcion.eventoId;
         selecParticipante.value = inscripcion.participanteId;
-        
-        if (selecEstadoModal) {
-            selecEstadoModal.value = inscripcion.estado;
-        }
 
-        
         btnGuardar.textContent = "Editar";
         btnGuardar.removeEventListener("click", crearInscripcion);
-        
-        
+
         btnGuardar.onclick = async () => {
             const eventoIdEdit = selecEvento.value;
             const participanteIdEdit = selecParticipante.value;
-            const estadoEdit = selecEstadoModal ? selecEstadoModal.value : "Confirmado";
 
-            
             try {
-                const validacion = await axios.get(`${API_URL}/inscripciones?eventoId=${eventoIdEdit}&participanteId=${participanteIdEdit}`);
-                const duplicado = validacion.data.find(insc => insc.id !== idInscripcion);
+                const validacion = await axios.get(
+                    `${API_URL}/inscripciones?eventoId=${eventoIdEdit}&participanteId=${participanteIdEdit}`
+                );
+
+                const duplicado = validacion.data.find(
+                    insc => insc.id !== idInscripcion
+                );
 
                 if (duplicado) {
-                    alert('Este participante ya se encuentra inscripto en este evento.');
+                    alert(
+                        "Este participante ya se encuentra inscripto en este evento."
+                    );
                     return;
                 }
             } catch (error) {
@@ -250,27 +243,35 @@ async function actualizarAsistencia(idInscripcion) {
             }
 
             try {
-                await axios.patch(`${API_URL}/inscripciones/${idInscripcion}`, {
-                    eventoId: eventoIdEdit,
-                    participanteId: participanteIdEdit,
-                    estado: estadoEdit // Ahora sí mandamos el estado nuevo
-                });
+                await axios.patch(
+                    `${API_URL}/inscripciones/${idInscripcion}`,
+                    {
+                        eventoId: eventoIdEdit,
+                        participanteId: participanteIdEdit
+                    }
+                );
 
-                
                 await mostrarInscripciones();
-                
-                
-                const modal = bootstrap.Modal.getOrCreateInstance(document.getElementById("modalInscripcion"));
-                modal.hide();
-                alert('El estado de la inscripción se modifico correctamente')
 
+                const modal = bootstrap.Modal.getOrCreateInstance(
+                    document.getElementById("modalInscripcion")
+                );
+
+                modal.hide();
+
+                alert("La inscripción se modificó correctamente");
             } catch (error) {
                 console.log("Error al hacer el PATCH:", error);
-                alert("Hubo un problema al guardar los cambios en el servidor.");
+                alert(
+                    "Hubo un problema al guardar los cambios en el servidor."
+                );
             }
         };
 
-        const modal = bootstrap.Modal.getOrCreateInstance(document.getElementById("modalInscripcion"));
+        const modal = bootstrap.Modal.getOrCreateInstance(
+            document.getElementById("modalInscripcion")
+        );
+
         modal.show();
 
     } catch (error) {
@@ -279,19 +280,19 @@ async function actualizarAsistencia(idInscripcion) {
     }
 }
 
-
-
-
-
-//  Eliminar  inscripción 
 async function cancelarInscripcion(idInscripcion) {
-    const confirmacion = confirm("¿Estás seguro que desea cancelar y eliminar esta inscripción?");
-    if (!confirmacion) return; 
+    const confirmacion = confirm(
+        "¿Estás seguro que desea cancelar y eliminar esta inscripción?"
+    );
+
+    if (!confirmacion) return;
+
     try {
-        await axios.delete(`${API_URL}/inscripciones/${idInscripcion}`);
-        
-        
-        await mostrarInscripciones(); 
+        await axios.delete(
+            `${API_URL}/inscripciones/${idInscripcion}`
+        );
+
+        await mostrarInscripciones();
 
     } catch (error) {
         console.log("Error al cancelar la inscripción:", error);
